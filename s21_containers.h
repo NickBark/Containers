7318c88b5d32f8baa4c515a058391d8dc21f23f1
Bober <thedillerthe@gmail.com>
2023-10-24 16:31:31 +0300

    first commit

---
+++ s21_containers.h
@@ -0,0 +1,6 @@
+#ifndef S21_CONTAINERS_H
+#define S21_CONTAINERS_H
+
+#include "s21_list.h"
+
+#endif // S21_CONTAINERS_H
---
+++ s21_list.h
@@ -0,0 +1,11 @@
+#ifndef S21_LIST_H
+#define S21_LIST_H
+
+namespace s21 {
+template <typename T> class List {
+  public:
+  private:
+};
+} // namespace s21
+
+#endif // S21_LIST_H
