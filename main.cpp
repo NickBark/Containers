3d51db90c01f989ebc92cd790a2027ae2d57a85d
Bober <thedillerthe@gmail.com>
2023-10-26 02:53:38 +0300

    list progress

--- main.cpp
+++ main.cpp
@@ -1,16 +1,40 @@
-#include <list>
-
 #include "s21_containers.h"
 
 int main() {
-    s21::list<std::string> list;
-    list.push_back("a");
-    list.push_back("b");
-    list.push_back("c");
+    s21::list<int> ppp = {1, 2, 3, 4};
+    s21::list<int> qqq(ppp);
+    // const s21::list<int> lll = {1, 2, 2, 3};
+    // auto it = lll.begin();
+    ppp[0] = 3;
+    ppp[1] = 3;
+    qqq.print_list();
+    ppp.print_list();
+    s21::list<int>::iterator it = ppp.begin();
+    // list.push_back("a");
+    // list.push_back("b");
+    // list.push_back("c");
     // list.print_list();
 
-    std::cout << list.front() << std::endl;
+    std::cout << std::endl << ppp.max_size() << std::endl;
+    // std::cout << "num of elem " << list.size() << std::endl;
 
+    // auto it = list.begin();
+    // auto it2 = list.begin();
+    // s21::list<std::string>::iterator it2 = list.begin();
+    // std::cout << *(++it) << std::endl;
+    // try {
+    //     // it++;
+    //     // it++;
+    //     // it++;
+    //     // it++;
+    //     // it--;
+    //     std::cout << *it2 << std::endl;
+    // } catch (std::exception& e) {
+    //     std::cout << e.what() << std::endl;
+    // }
+    // std::cout << *it++ << std::endl;
+    // std::cout << *it << std::endl;
+    // it++;
     // int a = 5;
     // int b = 6;
     // int *p1 = &a;
--- s21_list.h
+++ s21_list.h
@@ -3,6 +3,7 @@
 
 #include <cstddef>   // size_t
 #include <iostream>  // cout
+#include <limits>
 
 namespace s21 {
 template <typename T>
@@ -11,9 +12,26 @@ class list {
     using value_type = T;
     using reference = T&;
     using const_reference = const T&;
-    // using iterator = ListIterator<T>;
-    // using const_iterator = ListConstIterator<T>;
     using size_type = size_t;
+
+    list();
+    ~list();
+    list(size_type n);
+    list(std::initializer_list<value_type> const& items);
+    list(const list& l);
+    list(list&& l);
+
+    void push_back(const_reference value);
+    // void push_front(const_reference value);
+
+    void print_list();
+    size_type size() { return sizeOfList; }
+    size_type max_size() const { return std::numeric_limits<size_type>::max(); }
+
+    const_reference front() const { return head->data; }
+    const_reference back() const { return tail->data; }
+
+    reference operator[](const int& index);
 
    private:
     template <typename value_type>
@@ -32,32 +50,80 @@ class list {
     Node<T>* tail;
     size_type sizeOfList;
 
+    template <typename value_type>
+    class ListIterator {
+       protected:
+        Node<value_type>* cur;
+
+        ListIterator& operator=(ListIterator& other) {
+            cur = other.cur;
+            return *this;
+        }
+
+       public:
+        ListIterator(Node<value_type>* it) : cur(it) {}
+        reference operator*() const { return cur->data; }
+        ListIterator& operator++() {
+            if (!cur->pNext) throw std::out_of_range("End of list");
+            cur = cur->pNext;
+            return *this;
+        }
+
+        ListIterator operator++(int) {
+            ListIterator<value_type> it = *this;
+            if (!cur->pNext) throw std::out_of_range("End of list");
+            cur = cur->pNext;
+            return it;
+        }
+
+        ListIterator& operator--() {
+            cur = cur->pPrev;
+            return *this;
+        }
+
+        // ListIterator operator--(int);
+        ListIterator operator--(int) {
+            ListIterator<value_type> it = *this;
+            cur = cur->pPrev;
+            return it;
+        }
+
+        bool operator==(const ListIterator& other) { return cur == other.cur; }
+        bool operator!=(const ListIterator& other) { return cur != other.cur; }
+    };
+
+    template <typename value_type>
+    class ListConstIterator : public ListIterator<value_type> {
+       public:
+        ListConstIterator(const Node<value_type>* it)
+            : ListIterator<value_type>(const_cast<Node<T>*>(it)) {}
+
+        const_reference operator*() const { return this->cur->data; }
+    };
+
    public:
-    list();
-    ~list();
+    using iterator = ListIterator<T>;
+    using const_iterator = ListConstIterator<T>;
 
-    void push_back(const_reference value);
-    void push_front(const_reference value);
+    iterator begin() {
+        if (!head) throw std::out_of_range("The list has no elements");
+        return ListIterator(head);
+    }
 
-    void print_list();
-    size_type size();
+    const_iterator begin() const {
+        if (!head) throw std::out_of_range("The list has no elements");
+        return ListConstIterator(head);
+    }
 
-    const_reference front() const;
-    const_reference back() const;
+    iterator end() {
+        if (!tail) throw std::out_of_range("The list has no elements");
+        return ListIterator(tail->pNext);
+    }
 
-    // list(size_type n) {}
-    // list(std::initializer_list<value_type> const& items) {}
-    // list(const list& l) {}
-    // list(list&& l) {}
-
-    // list operator=(list&& l) {}
-
-    // iterator begin();
-    // iterator end();
-
-    // class iterator {
-    // ;
-    // };
+    const_iterator end() const {
+        if (!tail) throw std::out_of_range("The list has no elements");
+        return ListIterator(tail->pNext);
+    }
 };
 
 template <typename value_type>
@@ -80,21 +146,6 @@ void list<value_type>::push_back(const_reference value) {
 }
 
 template <typename value_type>
-typename list<value_type>::const_reference list<value_type>::front() const {
-    return head->data;
-}
-
-template <typename value_type>
-const value_type& list<value_type>::back() const {
-    return tail->data;
-}
-
-template <typename value_type>
-typename list<value_type>::size_type list<value_type>::size() {
-    return sizeOfList;
-}
-
-template <typename value_type>
 void list<value_type>::print_list() {
     std::cout << "start print list" << std::endl;
     Node<value_type>* current = head;
@@ -102,6 +153,52 @@ void list<value_type>::print_list() {
         std::cout << current->data << std::endl;
         current = current->pNext;
     }
+}
+
+template <typename value_type>
+list<value_type>::list(size_type n)
+    : sizeOfList(0), head(nullptr), tail(nullptr) {
+    for (size_type i = 0; i < n; i++) {
+        push_back(value_type());
+    }
+}
+
+template <typename value_type>
+typename list<value_type>::reference list<value_type>::operator[](
+    const int& index) {
+    if (index < 0 || index >= sizeOfList)
+        throw std::out_of_range("Out of list");
+    Node<value_type>* cur = head;
+    for (size_type i = 0; i < index; i++) {
+        cur = cur->pNext;
+    }
+    return cur->data;
+}
+
+template <typename value_type>
+list<value_type>::list(std::initializer_list<value_type> const& items)
+    : sizeOfList(0), head(nullptr), tail(nullptr) {
+    for (const_reference item : items) {
+        push_back(item);
+    }
+}
+
+template <typename value_type>
+list<value_type>::list(const list& l)
+    : sizeOfList(0), head(nullptr), tail(nullptr) {
+    const_iterator it = l.begin();
+    for (size_type i = 0; i < l.sizeOfList - 1; i++) {
+        push_back(*it++);
+    }
+    push_back(*it);
+}
+
+template <typename value_type>
+list<value_type>::list(list&& l)
+    : sizeOfList(l.sizeOfList), head(l.head), tail(l.tail) {
+    l.sizeOfList = 0;
+    l.head = nullptr;
+    l.tail = nullptr;
 }
 
 }  // namespace s21
