f965fca93d2ef38c4696dc1bf8634df87c655c2d
Bober <thedillerthe@gmail.com>
2023-10-25 15:20:15 +0300

    list progress

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
-#include <iostream>
+#include <list>
 
-int main() { return 0; }
+#include "s21_containers.h"
+
+int main() {
+    s21::list<std::string> list;
+    list.push_back("a");
+    list.push_back("b");
+    list.push_back("c");
+    // list.print_list();
+
+    std::cout << list.front() << std::endl;
+
+    // int a = 5;
+    // int b = 6;
+    // int *p1 = &a;
+    // int *p2 = p1;
+    // p1 = &b;
+    // std::cout << *p1 << " " << *p2 << std::endl;
+
+    return 0;
+}
--- s21_list.h
+++ s21_list.h
@@ -1,11 +1,109 @@
 #ifndef S21_LIST_H
 #define S21_LIST_H
 
-namespace s21 {
-template <typename T> class List {
-  public:
-  private:
-};
-} // namespace s21
+#include <cstddef>   // size_t
+#include <iostream>  // cout
 
-#endif // S21_LIST_H
+namespace s21 {
+template <typename T>
+class list {
+   public:
+    using value_type = T;
+    using reference = T&;
+    using const_reference = const T&;
+    // using iterator = ListIterator<T>;
+    // using const_iterator = ListConstIterator<T>;
+    using size_type = size_t;
+
+   private:
+    template <typename value_type>
+    class Node {
+       public:
+        Node(const_reference value = value_type(), Node* next = nullptr,
+             Node* prev = nullptr)
+            : data(value), pNext(next), pPrev(prev) {}
+
+        value_type data;
+        Node* pNext;
+        Node* pPrev;
+    };
+
+    Node<T>* head;
+    Node<T>* tail;
+    size_type sizeOfList;
+
+   public:
+    list();
+    ~list();
+
+    void push_back(const_reference value);
+    void push_front(const_reference value);
+
+    void print_list();
+    size_type size();
+
+    const_reference front() const;
+    const_reference back() const;
+
+    // list(size_type n) {}
+    // list(std::initializer_list<value_type> const& items) {}
+    // list(const list& l) {}
+    // list(list&& l) {}
+
+    // list operator=(list&& l) {}
+
+    // iterator begin();
+    // iterator end();
+
+    // class iterator {
+    // ;
+    // };
+};
+
+template <typename value_type>
+list<value_type>::list() : sizeOfList(0), head(nullptr), tail(nullptr) {}
+
+template <typename value_type>
+list<value_type>::~list() {}
+
+template <typename value_type>
+void list<value_type>::push_back(const_reference value) {
+    if (!head) {
+        head = new Node<value_type>(value);
+        tail = head;
+    } else {
+        tail->pNext = new Node<value_type>(value);
+        tail->pNext->pPrev = tail;
+        tail = tail->pNext;
+    }
+    sizeOfList++;
+}
+
+template <typename value_type>
+typename list<value_type>::const_reference list<value_type>::front() const {
+    return head->data;
+}
+
+template <typename value_type>
+const value_type& list<value_type>::back() const {
+    return tail->data;
+}
+
+template <typename value_type>
+typename list<value_type>::size_type list<value_type>::size() {
+    return sizeOfList;
+}
+
+template <typename value_type>
+void list<value_type>::print_list() {
+    std::cout << "start print list" << std::endl;
+    Node<value_type>* current = head;
+    while (current) {
+        std::cout << current->data << std::endl;
+        current = current->pNext;
+    }
+}
+
+}  // namespace s21
+
+#endif  // S21_LIST_H
