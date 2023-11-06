14dc2c297625fa97635e43e6661528556941d008
Bober <thedillerthe@gmail.com>
2023-11-06 18:57:48 +0300

    map iter

--- s21_map.h
+++ s21_map.h
@@ -3,6 +3,7 @@
 
 #include <cstddef>   // size_t
 #include <iostream>  // cout
+#include <limits>    // initializer_listq
 
 namespace s21 {
 template <typename Key, typename T>
@@ -31,12 +32,39 @@ class map {
     size_type size_;
 
     map();
+    map(std::initializer_list<value_type> const& items);
 
     void clear();
+    inline bool empty() const { return !size_; }
+    inline size_type size() const { return size_; }
+    size_type max_size() const { return std::numeric_limits<size_type>::max(); }
+
+    template <typename key_type, typename mapped_type>
+    class MapIterator {
+       private:
+        Node<key_type, mapped_type>* current_;
+
+       public:
+        MapIterator(Node<key_type, mapped_type>* node) : current_(node) {}
+        reference operator*() const noexcept { return current_->data_; }
+        MapIterator operator++(int) {}
+        MapIterator operator++() {}
+
+        bool operator==(const MapIterator& other) const noexcept {
+            return current_ == other.current_;
+        }
+        bool operator!=(const MapIterator& other) const noexcept {
+            return current_ != other.current_;
+        }
+    };
 };
 
 template <typename key_type, typename mapped_type>
-map<key_type, mapped_type>::map() : root(nullptr), size_(0) {}
+inline map<key_type, mapped_type>::map() : root(nullptr), size_(0) {}
+
+template <typename key_type, typename mapped_type>
+map<key_type, mapped_type>::map(
+    std::initializer_list<value_type> const& items) {}
 
 template <typename key_type, typename mapped_type>
 void map<key_type, mapped_type>::clear() {}
